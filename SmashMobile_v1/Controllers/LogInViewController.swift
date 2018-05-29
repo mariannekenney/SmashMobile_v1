bfe9e805874c1091f7a1dd46b0464243916194a5
Marianne Kenney <mkenney@AD-Mac.local>
2018-05-29 14:31:21 -0400

    First commit

--- SmashMobile_v1/Controllers/LogInViewController.swift
+++ SmashMobile_v1/Controllers/LogInViewController.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2018 SmashFly. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
