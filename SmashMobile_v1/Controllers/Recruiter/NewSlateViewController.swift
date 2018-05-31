87cbdee163e9e8e97a9aa056420a13d0b132ef0a
Marianne Kenney <mkenney@AD-Mac.local>
2018-05-31 12:36:06 -0400

    Changes, moving onto v2

---
+++ SmashMobile_v1/Controllers/Recruiter/NewSlateViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewSlateViewController.swift
+//  SmashMobile_v1
+//
+//  Created by Marianne Kenney on 5/31/18.
+//  Copyright © 2018 SmashFly. All rights reserved.
+//
+
+import UIKit
+
+class NewSlateViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ SmashMobile_v1/Controllers/Recruiter/RecievedTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RecievedTableViewController.swift
+//  SmashMobile_v1
+//
+//  Created by Marianne Kenney on 5/31/18.
+//  Copyright © 2018 SmashFly. All rights reserved.
+//
+
+import UIKit
+
+class RecievedTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
